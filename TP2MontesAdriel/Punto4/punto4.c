8ccdad1750873aded8c9afdc71c2a2aac7145385
Adriel-M-A <montes.adriel10@gmail.com>
2022-04-07 17:40:42 -0300

    main

--- TP2MontesAdriel/Punto4/punto4.c
+++ TP2MontesAdriel/Punto4/punto4.c
@@ -5,6 +5,14 @@
 
 char*comando[3]={"/bin/ls","-ltr", NULL};
 
+/*
+	4a) Que Hace?
+	Lo que hace el programa, es crear un nuevo proceso con el fork, si el valor que devuelve es 0 entonces
+	es ejecutado por el hijo, que abre una nueva ventana de Firefox. De lo contrario si el valor es 1, quiere
+	decir que es ejecutado por el padre; éste imprime que es el padre y espera a que el hijo termine de
+	ejecutarse para terminar el proceso.
+*/
+
 int main() {
 	int status;
 
