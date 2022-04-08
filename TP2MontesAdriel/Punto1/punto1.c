f8b6857e0b003045aac70fd021f3d7316e57427b
Adriel-M-A <montes.adriel10@gmail.com>
2022-04-08 15:51:16 -0300

    main

--- TP2MontesAdriel/Punto1/punto1.c
+++ TP2MontesAdriel/Punto1/punto1.c
@@ -3,6 +3,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+   1a) Que hace?
+   Espera que se ingrese la señal correspondiente para ser ejecutada por el manejador, en este caso la señal es de 
+   interrupcion "ctrl + c". Una vez que el ingreso de esta señal llega a la cantidad de veces requerida, finaliza 
+   la ejecución del programa y vuelve al shell normalmente.
+
+   1b1) 
+
+   1b2) si, el manejador se pierde al terminar el programa
+
+   1b3) Esposiblle capturar el SIGKILL mediante kill -9
+*/
+
 int cant = 0;
 void manejador(int num) {
    printf("Recibí señal\n");
