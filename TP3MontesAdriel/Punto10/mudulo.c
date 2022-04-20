41389e97cb7119a0891744c36a3afafc2697ead7
Adriel-M-A <montes.adriel10@gmail.com>
2022-04-20 12:30:25 -0300

    main

---
+++ TP3MontesAdriel/Punto10/mudulo.c
@@ -0,0 +1,13 @@
+
+
+
+/* TODO
+10.Implemente un pequeño ejemplo de un módulo funcional en lenguaje C, indicando
+los pasos necesarios para su creación. El módulo puede ser algo tan sencillo como
+mostrar un mensaje al cargarlo y otro al descargarlo.
+*/
+
+
+int main(){
+    
+}
