15689660b98ceaecf113681aabb9754627012c7a
Adriel_M_A <montes.adriel10@gmail.com>
2022-04-21 15:24:57 -0300

    main

--- TP3MontesAdriel/Punto10/modulo.c
+++ TP3MontesAdriel/Punto10/modulo.c
@@ -1,6 +1,12 @@
+// Definiendo __KERNEL__ and MODULE nos permite acceder a nivel de kernel.
+//root@pc#undef __KERNEL__
+//root@pc#define __KERNEL__
+//root@pc#undef MODULE
+//root@pc#define MODULE
+
 #include <linux/module.h> /* Lib para modulos */
 #include <linux/kernel.h> /* Para usar la macro KERN_INFO */
-#include <linux/init.h> /* Para usar la macro de inicio y fin */
+#include <linux/init.h> /* Para usar la macro de __init y __exit */
 
 /*
 10.Implemente un pequeño ejemplo de un módulo funcional en lenguaje C, indicando
@@ -8,13 +14,14 @@ los pasos necesarios para su creación. El módulo puede ser algo tan sencillo c
 mostrar un mensaje al cargarlo y otro al descargarlo.
 */
 
-static init __init_mi_modulostart(void){
+static int __init mi_modulostart(void){
     printk(KERN_INFO "hola mundo");
+    return 0;
 }
 
-static exit __exit_mi_moduloexit(void){
+static void __exit mi_moduloexit(void){
     printk(KERN_INFO "chau mundo");
 }
 
-module_init(__init_mi_modulostart);
-module_exit(__exit_mi_moduloexit);
+module_init(mi_modulostart);
+module_exit(mi_moduloexit);
