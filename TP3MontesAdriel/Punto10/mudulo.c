f378640baf45d018d44f185b8afb659f4ea86fff
Adriel_M_A <montes.adriel10@gmail.com>
2022-04-21 15:00:02 -0300

    main

--- TP3MontesAdriel/Punto10/mudulo.c
+++ TP3MontesAdriel/Punto10/mudulo.c
@@ -7,3 +7,12 @@
 los pasos necesarios para su creación. El módulo puede ser algo tan sencillo como
 mostrar un mensaje al cargarlo y otro al descargarlo.
 */
+
+static init __init_mi_modulostart(void){
+    printk(KERN_INFO "hola mundo");
+}
+
+static exit __exit_mi_moduloexit(void){
+    printk(KERN_INFO "chau mundo");
+}
+
