5e1e36dd3d45e1b1d3c5b65a6c2b7f96dd9fed48
Adriel-M-A <montes.adriel10@gmail.com>
2022-04-26 12:39:28 -0300

    main

---
+++ TP4/servidor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "servidor.h"
+#include "interfaz.h"
+#include "comunicaciones.h"
+
+
+void Servidor(){
+    printf("Hola soy el servidor\n");
+}
+
+int main(){
+    Servidor();
+}         
