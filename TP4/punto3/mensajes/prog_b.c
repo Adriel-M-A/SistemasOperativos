c44a7ab23671ede0b9af970ddefc24a11229a943
Adriel-M-A <montes.adriel10@gmail.com>
2022-05-15 20:46:20 -0300

    main

--- TP4/punto3/mensajes/prog_b.c
+++ TP4/punto3/mensajes/prog_b.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -9,13 +8,25 @@
 int main (){
 
 	mqd_t cola_mensajes;
+	//char c_buffer[MAX+1];
 	char* c_buffer;
 	struct mq_attr atributos;
 	
+//definimos los atributos de la cola de mensajes
+/*
+	atributos.mq_flags = 0;
+	atributos.mq_maxmsg = 15;
+	atributos.mq_msgsize = MAX;
+	atributos.mq_curmsgs = 0;
+
+*/	
+	//if((cola_mensajes = mq_open("/colaMensajes", O_RDONLY, &atributos)) == -1){
 	if((cola_mensajes = mq_open("/mq_so", O_RDWR)) == -1){
 		perror("No se pudo leer cola de mensajes");
 		return 1;
 	}
+
+
 
 	while(1){
 
@@ -24,7 +35,10 @@ int main (){
 			perror("No se pudo recibir el mensaje");
 			return -1;
 		}
+
+		//if(*c_buffer == EOF) break;
 		
+		//printf("%c", *c_buffer);
 		printf("%s", c_buffer);
 	}
 
